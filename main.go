e43c245a173617d979765dd9755f9b60a6f02b1d
miftahayu <miftahayu@mail.ugm.ac.id>
2022-01-03 10:43:29 +0700

    validate

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main (){
+	r := gin.Default()
+
+	type authheader struct{
+		AuthorizationHeader string `header:"Authorization"`
+	}
+
+	r.GET("/customer", func(c *gin.Context) {
+		h := authheader{}
+
+		if err := c.ShouldBindHeader(&h); err !=nil{
+			c.JSON(401,gin.H{
+				"message":"Unauthorized",
+			})
+			return
+		}
+
+		if h.AuthorizationHeader=="123"{
+			c.JSON(200,gin.H{
+				"message":"customer",
+			})
+			return
+		}
+		c.JSON(401,gin.H{
+			"message":"Unauthorized",
+		})
+	})
+
+	r.GET("/product", func(c *gin.Context) {
+		h := authheader{}
+
+		if err := c.ShouldBindHeader(&h); err !=nil{
+			c.JSON(401,gin.H{
+				"message":"Unauthorized",
+			})
+			return
+		}
+
+		if h.AuthorizationHeader=="123"{
+			c.JSON(200,gin.H{
+				"message":"product",
+			})
+			return
+		}
+		c.JSON(401,gin.H{
+			"message":"Unauthorized",
+		})
+	})
+	
+	err := r.Run("localhost:8888")
+	if err !=nil{
+		panic(err)
+	}
+}
