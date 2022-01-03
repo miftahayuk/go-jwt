33c45b65435659c96f50bd6636e961fc01156585
miftahayu <miftahayu@mail.ugm.ac.id>
2022-01-03 11:05:02 +0700

    login

--- main.go
+++ main.go
@@ -2,57 +2,108 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main (){
-	r := gin.Default()
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-	type authheader struct{
-		AuthorizationHeader string `header:"Authorization"`
-	}
+type authheader struct {
+	AuthorizationHeader string `header:"Authorization"`
+}
+
+func main() {
+	r := gin.Default()
+	r.Use(AuthtokenMiddleware())
+
+	r.POST("/login", func(c *gin.Context) {
+		var user Credential
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(400, gin.H{
+				"message": "can't bind struct",
+			})
+			return
+		}
+
+		if user.Username == "enigma" && user.Password == "123" {
+			c.JSON(200, gin.H{
+				"token": "123",
+			})
+		} else {
+			c.AbortWithStatus(401)
+		}
+	})
 
 	r.GET("/customer", func(c *gin.Context) {
 		h := authheader{}
 
-		if err := c.ShouldBindHeader(&h); err !=nil{
-			c.JSON(401,gin.H{
-				"message":"Unauthorized",
+		if err := c.ShouldBindHeader(&h); err != nil {
+			c.JSON(401, gin.H{
+				"message": "Unauthorized",
 			})
 			return
 		}
 
-		if h.AuthorizationHeader=="123"{
-			c.JSON(200,gin.H{
-				"message":"customer",
+		if h.AuthorizationHeader == "123" {
+			c.JSON(200, gin.H{
+				"message": "customer",
 			})
 			return
 		}
-		c.JSON(401,gin.H{
-			"message":"Unauthorized",
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
 		})
 	})
 
 	r.GET("/product", func(c *gin.Context) {
 		h := authheader{}
 
-		if err := c.ShouldBindHeader(&h); err !=nil{
-			c.JSON(401,gin.H{
-				"message":"Unauthorized",
+		if err := c.ShouldBindHeader(&h); err != nil {
+			c.JSON(401, gin.H{
+				"message": "Unauthorized",
 			})
 			return
 		}
 
-		if h.AuthorizationHeader=="123"{
-			c.JSON(200,gin.H{
-				"message":"product",
+		if h.AuthorizationHeader == "123" {
+			c.JSON(200, gin.H{
+				"message": "product",
 			})
 			return
 		}
-		c.JSON(401,gin.H{
-			"message":"Unauthorized",
+		c.JSON(401, gin.H{
+			"message": "Unauthorized",
 		})
 	})
-	
+
 	err := r.Run("localhost:8888")
-	if err !=nil{
+	if err != nil {
 		panic(err)
+	}
+}
+
+func AuthtokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.URL.Path == "/login" {
+			c.Next()
+		} else {
+			h := authheader{}
+
+			if err := c.ShouldBindHeader(&h); err != nil {
+				c.JSON(401, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+			}
+
+			if h.AuthorizationHeader == "123" {
+				c.Next()
+			} else {
+				c.JSON(401, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+			}
+		}
 	}
 }
